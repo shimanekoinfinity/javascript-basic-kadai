8df97687fd19c6606deed5e04a3e97c01fbb00ed
たつひこ <135490872+shimanekoinfinity@users.noreply.github.com>
2023-06-29 17:54:28 +0900

    Add files via upload

--- kadai_019/event.js
+++ kadai_019/event.js
@@ -1,17 +1,6 @@
-//HTML要素がクリックされたときにイベント処理を実行する
+function changeButton(){
+  document.getElementById('text').textContent = 'ボタンをクリックしました';
+}
+document.getElementById('btn').addEventListener('click',changeButton);
 
-const btn = document.getElementById("button");
-console.log(btn);
-const text = document.getElementById("js-text");
-console.log(text);
- const changeText =()=>{
- //  text.textContent="ボタンをクリックしました！"
-   btn.style.background = '#0091EA';
- }
-btn.addEventListener('mouseover', changeText);
 
-btn.addEventListener('mouseleave', () => {
-
-  btn.style.background = '';
-
-}, false)
