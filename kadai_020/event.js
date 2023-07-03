b35fa1e597cebc9e5375b162e7475f35ff0e65db
たつひこ <135490872+shimanekoinfinity@users.noreply.github.com>
2023-07-03 15:38:21 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,15 @@
-setTimeout(() => {
-  document.getElementById('text').textContent = 'ボタンをクリックしました';
-}, 2000);
-document.getElementById('btn').addEventListener('click',changeButton);
+const timer = () => {
+  console.log(4649);
+  setTimeout(changeText,2000);
+}
 
+const changeText = () => {
+  console.log(1111);
+  document.getElementById('text').textContent = 'ボタンをクリックしました';
+}
+
+
+const btn = document.getElementById('btn').addEventListener('click',timer)
+console.log(btn);
 
 
